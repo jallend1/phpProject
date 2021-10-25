fff2626f3a53325b83bc103d2c059152ee0850b7
jallend1 <jallend1@gmail.com>
2021-10-25 16:50:03 -0700

    Gets the connection to the database rocking proper

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
     if(!$conn){
         echo 'Connection error: ' . mysqli_connect_error();
     }
+    // Write the query for all the pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    // Actually make the query and gets the result
+    $result = mysqli_query($conn, $sql);
+    // Fetch the results in an arry
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    // Frees result from memory
+    mysqli_free_result($result);
+    // Closes connection
+    mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
